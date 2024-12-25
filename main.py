024ad913c62714d747fc9fa63f5b01fe6326a59f
nphylxs <130030842+nphylxs@users.noreply.github.com>
2024-12-25 18:53:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from ai_functions import reddit_scraper, analyser, reader
+
+def main():
+    product = input("Product Name: ").lower().strip()
+    print("Type the subreddits whose comments you want to read. \nOnce you're done, input \"Search\" to begin search. ")
+    subs = []
+    while True:
+        name = input("Subreddit name: ").strip()
+        if name.lower() == "search":
+            break
+        subs.append(name)
+
+    if subs:
+        for sub in subs:
+            filename = f"data/{sub}_{product.replace(' ', '_')}.json"
+            print(f"Searching r/{sub}")
+            if not os.path.exists(filename):
+                reddit_scraper(product, filename, sub)
+            print("Done!")
+
+    else:
+        print("No subreddit entered. Searching the default subreddit, r/SkincareAddiction")
+        filename = f"data/SkincareAddiction_{product.replace(' ', '_')}.json"
+        if not os.path.exists(filename):
+            reddit_scraper(product, filename)
+        print("Done!")
+        subs.append("SkincareAddiction")
+    
+    print("Starting the analyzing process!")
+    for sub in subs:
+        saving = f"reviews/{sub}_{product.replace(' ', '_')}.json"
+        if not os.path.exists(saving):
+            analyser(product, sub)
+        print(reader(saving))
+            
+if __name__ == "__main__":
+    main()
